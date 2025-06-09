ac7c926e31a4658223da7c55c8f33e1190b2ca69
Gadzhiev Ahmed <akhmed.34@mail.ru>
2025-06-09 16:14:35 +0300

    Добавлен базовый класс для отрисовки сцены

---
+++ core/src/main/kotlin/org/cluelesshog/game/Scene.kt
@@ -0,0 +1,77 @@
+package org.cluelesshog.game
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import ktx.app.KtxScreen
+
+abstract class Scene (
+    protected val wrapper: Stage = Stage(ScreenViewport())
+) : KtxScreen {
+    private var isVisible = false
+
+    private var isLoaded = false
+
+    /**
+     *  Сцена должна возвращать true при успешном завершении загрузки и false при неудачном
+     *  Таким образом мы предотвращаем всевозможные ошибки, которые возникают при переходах между сценами.
+     *  Например, когда сцена загрузки игры не находит сохранение и пытается вернуться в главное меню
+     */
+    protected abstract fun load(): Boolean
+
+    override fun show() {
+        if (!isLoaded) {
+            isLoaded = load()
+        }
+
+        if (!isLoaded) {
+            return
+        }
+
+        isVisible = true
+        wrapper.isDebugAll = Config.DEBUG_MODE
+        Gdx.input.inputProcessor = wrapper
+    }
+
+    override fun hide() {
+        isVisible = false
+        Gdx.input.inputProcessor = null
+    }
+
+    final override fun render(delta: Float) {
+        if (!isVisible) {
+            return
+        }
+
+        wrapper.act(delta)
+        wrapper.draw()
+    }
+
+    final override fun resize(width: Int, height: Int) {
+        if (!isVisible) {
+            return
+        }
+
+        wrapper.viewport.update(width, height, true)
+
+        onResize(width, height)
+    }
+
+    protected open fun onResize(newWidth: Int, newHeight: Int) {
+
+    }
+
+    override fun dispose() {
+        wrapper.dispose()
+    }
+
+    protected fun getScreenWidth(): Int
+    {
+        return wrapper.viewport.screenWidth
+    }
+
+    protected fun getScreenHeight(): Int
+    {
+        return wrapper.viewport.screenHeight
+    }
+}
