f60539262351856939c6d1c7d4817b899942e52d
Gadzhiev Ahmed <akhmed.34@mail.ru>
2025-06-24 13:16:46 +0300

    Удален ненужный метод

--- core/src/main/kotlin/org/cluelesshog/game/asset/TextureUtils.kt
+++ core/src/main/kotlin/org/cluelesshog/game/asset/TextureUtils.kt
@@ -1,27 +1,11 @@
 package org.cluelesshog.game.asset
 
 import asset.AssetLoader
-import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.Pixmap
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import org.cluelesshog.game.logic.JewelType
 
 object TextureUtils {
-    private val cache = HashMap<Color, Texture>()
-
-    private fun createSolidTexture(color: Color, width: Int = 1, height: Int = 1): Texture {
-        return cache.getOrPut(color) {
-            val pixmap = Pixmap(width, height, Pixmap.Format.RGBA8888)
-            pixmap.setColor(color)
-            pixmap.fill()
-            val texture = Texture(pixmap)
-            pixmap.dispose()
-            return texture
-        }
-    }
-
     fun loadTextureForJewelType(jewelType: JewelType): TextureRegionDrawable {
         val texture = when (jewelType) {
             JewelType.DIAMOND -> AssetLoader.getTile("diamond")
