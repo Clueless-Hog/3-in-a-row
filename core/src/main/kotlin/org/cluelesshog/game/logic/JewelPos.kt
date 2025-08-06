e3f68537f61c092853122383a8d87b873b889480
Ahmed <akhmed.34@mail.ru>
2025-08-06 17:21:15 +0300

    Правки

--- core/src/main/kotlin/org/cluelesshog/game/logic/JewelPos.kt
+++ core/src/main/kotlin/org/cluelesshog/game/logic/JewelPos.kt
@@ -1,10 +1,3 @@
 package org.cluelesshog.game.logic
 
-data class JewelPos(val column: Int, val row: Int) {
-    fun neighbors() = listOf(
-        JewelPos(column - 1, row),
-        JewelPos(column + 1, row),
-        JewelPos(column, row - 1),
-        JewelPos(column, row + 1)
-    )
-}
+data class JewelPos(val column: Int, val row: Int)
